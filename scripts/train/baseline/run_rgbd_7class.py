995206ce8c5d684eb3c3f663c89475ae55992036
rainkaze <rainkaze@qq.com>
2026-04-25 14:47:21 +0800

    🐛 修复文件指向错误

--- scripts/train/baseline/run_rgbd_7class.py
+++ scripts/train/baseline/run_rgbd_7class.py
@@ -9,4 +9,4 @@ from scripts.train.train_recognition import run_from_config
 
 
 if __name__ == "__main__":
-    run_from_config(Path("configs/train/casme3/rgbd_7class.toml"))
+    run_from_config(Path("configs/train/casme3/baseline/rgbd_7class.toml"))
